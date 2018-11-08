f9a3f68bd6fb733f77f10d498fac93616b04da28
Sanjeev <kamal00sanjeev@gmail.com>
2018-11-08 23:48:15 +0530

    updated testcase for books

---
+++ SpringBootRestApp/README.md
@@ -0,0 +1,7 @@
+Test:
+===========
+Get all the books - GET
+http://localhost:8080/BookApp/books
+
+Fetch a book details - GET
+http://localhost:8080/BookApp/books/{id}
--- SpringBootRestApp/src/test/java/com/boot/SpringBootRest/SpringBootRestApplicationTests.java
+++ SpringBootRestApp/src/test/java/com/boot/SpringBootRest/SpringBootRestApplicationTests.java
@@ -1,16 +1,35 @@
 package com.boot.SpringBootRest;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootRestApplicationTests {
 
+		
 	@Test
-	public void contextLoads() {
+	public void testGetBookListSuccess() throws URISyntaxException
+	{
+	    RestTemplate restTemplate = new RestTemplate();
+	     
+	    final String baseUrl = "http://localhost:8080/BookApp/books";
+	    URI uri = new URI(baseUrl);
+	 
+	    ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+	     
+	    System.out.println(result);
+	    //Verify request succeed
+	    Assert.assertEquals(200, result.getStatusCodeValue());
+	    Assert.assertEquals(false, result.getBody().contains("books"));
 	}
 
 }
