1731c1f17bcd0dbc22f5c1f4a78a29cae9c59b14
Sanjeev <kamal00sanjeev@gmail.com>
2019-08-10 23:29:05 +0530

    adding hello world gradle

---
+++ HelloWorldGradle/src/main/java/com/grdl/HelloWorldApp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.grdl;
+
+/**
+ * @author Sanjeev
+ *
+ */
+public class HelloWorldApp {
+
+	public HelloWorldApp() {
+		super();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World from Gradle!");
+	}
+
+}
